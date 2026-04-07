a273f1ec733570dfe9813bbcabf1b85155d663bd
DanteAvetyan <danteavetyan@gmail.com>
2026-04-06 22:10:06 -0700

    Late Update

---
+++ README.md
@@ -0,0 +1,15 @@
+# HW9
+
+# Link to GitHub:
+https://github.com/COMP484-Dante-Avetyan/HW9
+
+# Link to url:
+https://comp484-dante-avetyan.github.io/HW9/
+# My GitHub Pages is launched, but it's been inconsistent on if it works or not.
+
+# Reflection
+The most easiest part was being given the html, and not having to do a style.css file.
+The most difficult part was the javascript, although I already have experience, but I have to write an answer for it.
+I learned that the date object uses a 0 based index and how to add zeroes to a date if it's a under 10.
+For the Number object, I realized I either never learned it or did minimally and forgot. So it was new to me.
+For updating the data, it was very easy and simple. I actually did learn much since I already was aware of how to do it.
--- index.js
+++ index.js
@@ -1,1 +1,72 @@
-console.log('Happy developing ✨')
+
+
+// Part 1
+
+function displayDate() {
+    var today = new Date();
+
+    var month = today.getMonth() + 1;
+    var day = today.getDate();
+    var year = today.getFullYear();
+
+    if (month < 10) {
+        month = '0' + month;
+    }
+    if (day < 10) {
+        day = '0' + day;
+    }
+
+    document.getElementById('dateOutput').innerHTML = "Today is " + month + '/' + day + '/' + year;
+}
+
+function testNumber(val) {
+    var converted = Number(val);
+    var isNaN = Number.isNaN(converted);
+    var isInt = Number.isInteger(converted);
+
+    return "Value: '" + val + "'.&nbsp; Converted: " + converted
+        + ".&nbsp; isNaN: " + isNaN + ".&nbsp; isInteger: " + isInt;
+}
+
+function displayNumberConversions() {
+    var val1 = "98";
+    var val2 = "20.325";
+    var val3 = "goodbye";
+    var val4 = "2003";
+
+    document.getElementById("numberConversionOutput").innerHTML = testNumber(val1) + "<br>"
+        + testNumber(val2) + "<br>"
+        + testNumber(val3) + "<br>"
+        + testNumber(val4);
+}
+
+
+// Part 3
+
+function displayMathAndFormat() {
+    var itemPrice = 29.99;
+    var taxRate = 0.08;
+    var shipping = 5.00;
+
+    var subtotal = itemPrice + shipping;
+    var taxAmount = itemPrice * taxRate;
+    var total = subtotal + taxAmount;
+
+    var shippingMessage;
+    if (total > 40) {
+        shippingMessage = "You qualify for a loyalty discount next order!";
+    } else {
+        shippingMessage = "Add more items to qualify for a discount.";
+    }
+
+    document.getElementById("mathOutput").innerHTML = "Item Price: $" + itemPrice.toFixed(2)
+        + "<br>Shipping: $" + shipping.toFixed(2)
+        + "<br>Subtotal: $" + subtotal.toFixed(2)
+        + "<br>Tax (8%): $" + taxAmount.toFixed(2)
+        + "<br>Total: $" + total.toFixed(2)
+        + "<br>" + shippingMessage;
+}
+
+displayDate();
+displayNumberConversions();
+displayMathAndFormat();
